96b1e0b8f55ae9029f86976ef5f00c82718fff27
hierifer <thu4@dons.usfca.edu>
2019-07-17 00:48:22 +0800

    init

---
+++ ImmuneSystem/Assets/Script/main.cs
@@ -0,0 +1,11 @@
+using System;
+namespace printHelloCsharp
+ {
+   class HelloCsharp {
+      static void Main(string[] args) 
+      {
+         /* Print some string in C# */
+         Console.WriteLine("Hello C#.");
+      }
+   }
+}
